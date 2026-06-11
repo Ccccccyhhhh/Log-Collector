bc67802230f79b114c07c0af05cedf6f0f8c91c7
Ccccccyhhhh <2131919396@qq.com>
2026-06-12 06:55:38 +0800

    Add files via upload
    第一次上传 log_collect  code

---
+++ config.py
@@ -0,0 +1,67 @@
+
+# ================== 工位配置 ==================
+STATION={
+    #V64 TT
+    #ip=7" "139.224.223.13ser="user"
+
+    #for debug
+    "V64_TT_PreDFU": {"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_TT_PreDFU"},
+    "V64_TT_PreFCT": {"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_TT_PreFCT"},
+    "V64_TT_PostDFU":{"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_TT_PostDFU"},
+    "V64_TT_PostFCT":{"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_TT_PostFCT"},
+    "V64_TT_DMNS":{"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_TT_DMNS"},
+    "V64_TT_IBAT_RESET":{"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_TT_IBAT_RESET"},
+
+    #V64 CYG
+    "V64_CYG_PreDFU": {"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_CYG_PreDFU"},
+    "V64_CYG_PreFCT": {"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_CYG_PreFCT"},
+    "V64_CYG_PostDFU": {"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_CYG_PostDFU"},
+    "V64_CYG_PostFCT": {"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_CYG_PostFCT"},
+    "V64_CYG_DMNS": {"ip":"139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64_CYG_DMNS"},
+
+    #V64S TT
+    # "V64S_TT_PreDFU": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_TT_PreDFU"},
+    # "V64S_TT_PreFCT": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_TT_PreFCT"},
+    # "V64S_TT_PostDFU": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_TT_PostDFU"},
+    # "V64S_TT_PostFCT": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_TT_PostFCT"},
+    # "V64S_TT_DMNS": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_TT_DMNS"},
+    # "V64S_TT_IBAT_RESET": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_TT_IBAT_RESET"},
+
+
+    #V64S CYG
+    # "V64S_CYG_PreDFU": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_CYG_PreDFU"},
+    # "V64S_CYG_PreFCT": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_CYG_PreFCT"},
+    # "V64S_CYG_PostDFU": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_CYG_PostDFU"},
+    # "V64S_CYG_PostFCT": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_CYG_PostFCT"},
+    # "V64S_CYG_DMNS": {":""139.224.223.137","user":"root","remote_dir":"/home/cyh/log/V64S_CYG_DMNS"},
+
+}
+
+#本地根目录
+#LOCAL_ROOT="./smokey_logs"
+LOCAL_V64_LOG="./V64_log"
+LOCAL_V64S_LOG="./V64S_log"
+
+
+
+
+# ===================状态颜色定义===================
+STATUS = {
+    "wait": "lightgray",   # 未收集
+    "running": "#63afff", # 收集中
+    "success": "#90ed90", # 完成
+    "fail": "#ff7070"     # 失败
+}
+
+
+
+#======================站位信息========================
+TT_steps = [
+"PreDFU","PreFCT","PostDFU","PostFCT","DMNS","IBAT_RESET"
+]
+
+CYG_steps=[
+"PreDFU","PreFCT","PostDFU","PostFCT","DMNS"
+]
+
+
